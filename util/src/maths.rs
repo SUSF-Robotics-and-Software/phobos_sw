45a7fca656369b8f0fb7c5d7606fea0a8baa721b
Duncan Hamill <duncanrhamill@googlemail.com>
2020-05-07 17:43:07 +0100

    Added first version of traj ctrl, testing still needed

--- util/src/maths.rs
+++ util/src/maths.rs
@@ -16,3 +16,27 @@ where
         * (target_range.1 - target_range.0) 
         / (source_range.1 - source_range.0))
 }
+
+/// Return the euclidian norm (distance between) of two points.
+///
+/// If the points do not have the same number of dimentions then `None` is 
+/// returned.
+pub fn norm<T>(point_0: &[T], point_1: &[T]) -> Option<T> 
+where
+    T: Float + std::ops::AddAssign
+{
+    // Check that the dimentions match
+    if point_0.len() != point_1.len() {
+        return None;
+    }
+
+    // Sum all elements of the points
+    let mut sum = T::from(0).unwrap();
+
+    for i in 0..point_0.len() {
+        sum += (point_0[i] - point_1[i]).powi(2);
+    }
+
+    // Return the squareroot of the sum
+    Some(sum.sqrt())
+}
