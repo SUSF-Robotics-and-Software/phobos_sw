fcfaf9139e3fb5bd941fef7b364793a521891936
Duncan Hamill <duncanrhamill@googlemail.com>
2020-05-10 22:07:49 +0100

    Updated loco_ctrl_test_02 to add curvature

--- scripts/gen/gen_loco_ctrl_test_02.py
+++ scripts/gen/gen_loco_ctrl_test_02.py
@@ -20,6 +20,13 @@ def main():
         script += get_ack(time_s, 0.0, speed)
         time_s += 4.0
 
+    # Generate curvs from -6.5 to 6.5 (wheelbase to wheelbase)
+    curvs_m = np.linspace(-6.5, 6.5, 20)
+
+    for curv in curvs_m:
+        script += get_ack(time_s, curv, 0.0)
+        time_s += 4.0
+
     with open('scripts/loco_ctrl_test_02.prs', 'w') as f:
         f.write(script)
 
