f75070c280d41cb330b46c5f546e37bfead85f1f
Duncan Hamill <duncanrhamill@googlemail.com>
2020-05-12 17:53:13 +0100

    Update to lococtrl speed limits

--- scripts/gen/gen_loco_ctrl_test_02.py
+++ scripts/gen/gen_loco_ctrl_test_02.py
@@ -13,19 +13,19 @@ def main():
     time_s = 1.0
     script = '# Generated from scripts/gen/gen_loco_ctrl_text_02.py\n'
 
-    # Generate speeds from -1 to 1 m/s (20) with 4 s spacing
-    speeds_ms = np.linspace(-1.0, 1.0, 20)
+    # Generate speeds from -0.175 to 0.175 m/s (20) with 3 s spacing
+    speeds_ms = np.linspace(-0.175, 0.175, 20)
 
     for speed in speeds_ms:
         script += get_ack(time_s, 0.0, speed)
-        time_s += 4.0
+        time_s += 3.0
 
     # Generate curvs from -6.5 to 6.5 (wheelbase to wheelbase)
     curvs_m = np.linspace(-6.5, 6.5, 20)
 
     for curv in curvs_m:
         script += get_ack(time_s, curv, 0.0)
-        time_s += 4.0
+        time_s += 3.0
 
     with open('scripts/loco_ctrl_test_02.prs', 'w') as f:
         f.write(script)
