80d9faf2d95de06b57bf06b7b8c00a027ed064d6
Ceris Brown <james@impromptu-software.co.uk>
2020-09-27 14:41:04 +0100

    Update mechdems struct

--- comms_if/src/eqpt.rs
+++ comms_if/src/eqpt.rs
@@ -7,22 +7,49 @@
 // ------------------------------------------------------------------------------------------------
 
 use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
 // ------------------------------------------------------------------------------------------------
 // STRUCTS
 // ------------------------------------------------------------------------------------------------
 
-/// TODO
+
 #[derive(Serialize, Deserialize, Debug)]
-pub struct MechDems;
+pub struct MechDems {
+	pub pos_rad: HashMap<ActId, f64>,
+	pub speed_rad_s: HashMap<ActId, f64>
+}
 
 /// TODO
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MechSensData;
 
+
+
 // ------------------------------------------------------------------------------------------------
 // ENUMS
 // ------------------------------------------------------------------------------------------------
+
+#[derive(Serialize, Deserialize, Debug, Hash, Eq, PartialEq)]
+pub enum ActId {
+	DrvFL,
+	DrvML,
+	DrvRL,
+	DrvFR,
+	DrvMR,
+	DrvRR,
+	StrFL,
+	StrML,
+	StrRL,
+	StrFR,
+	StrMR,
+	StrRR,
+	ArmBase,
+	ArmShoulder,
+	ArmElbow,
+	ArmWrist,
+	ArmGrabber
+}
 
 /// Response from the mechanisms server based on the demands sent by the client.
 #[derive(Serialize, Deserialize, Debug)]
